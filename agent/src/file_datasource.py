42b2feb2717c87cd5472a3d8c84f3a92b3846dc4
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2025-03-17 16:02:56 +0200

    add: parking data

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -3,31 +3,39 @@ from datetime import datetime
 from domain.accelerometer import Accelerometer
 from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
+from domain.parking import Parking
 import config
 
 class FileDatasource:
-    def __init__(self, accelerometer_filename: str, gps_filename: str) -> None:
+    def __init__(self, accelerometer_filename: str, gps_filename: str, parking_filename: str) -> None:
         self.accelerometer_filename = accelerometer_filename
         self.gps_filename = gps_filename
+        self.parking_filename = parking_filename
         self.accel_file = None
         self.gps_file = None
+        self.parking_file = None
         self.accel_reader = None
         self.gps_reader = None
+        self.parking_reader = None
 
     def startReading(self):
         self.accel_file = open(self.accelerometer_filename, "r")
         self.gps_file = open(self.gps_filename, "r")
+        self.parking_file = open(self.parking_filename, "r")
 
         self.accel_reader = reader(self.accel_file)
         self.gps_reader = reader(self.gps_file)
+        self.parking_reader = reader(self.parking_file)
 
         next(self.accel_reader, None)
         next(self.gps_reader, None)
+        next(self.parking_reader, None)
 
-    def read(self) -> AggregatedData:
+    def read(self):
         try:
             accel_data = next(self.accel_reader)
             gps_data = next(self.gps_reader)
+            parking_data = next(self.parking_reader)
 
             accelerometer = Accelerometer(
                 x=int(accel_data[0]),
@@ -38,13 +46,21 @@ class FileDatasource:
                 longitude=float(gps_data[0]),
                 latitude=float(gps_data[1]),
             )
-
-            return AggregatedData(
-                accelerometer=accelerometer,
-                gps=gps,
-                time=datetime.now(),
-                user_id=config.USER_ID,
+            parking = Parking(
+                empty_count=int(parking_data[0]),
+                gps=gps
             )
+
+            return {
+                "aggregated_data": AggregatedData(
+                    accelerometer=accelerometer,
+                    gps=gps,
+                    time=datetime.now(),
+                    user_id=config.USER_ID,
+                ),
+                "parking_data": parking
+            }
+
         except StopIteration:
             return None
 
@@ -53,3 +69,5 @@ class FileDatasource:
             self.accel_file.close()
         if self.gps_file:
             self.gps_file.close()
+        if self.parking_file:
+            self.parking_file.close()
