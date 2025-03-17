d3558e942c8c19ded736a705dbe9ff603a67503e
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2025-03-17 16:00:08 +0200

    add: basic FileDatasouce

--- agent/src/file_datasource.py
+++ agent/src/file_datasource.py
@@ -5,26 +5,51 @@ from domain.gps import Gps
 from domain.aggregated_data import AggregatedData
 import config
 
-
 class FileDatasource:
-    def __init__(
-        self,
-        accelerometer_filename: str,
-        gps_filename: str,
-    ) -> None:
-        pass
+    def __init__(self, accelerometer_filename: str, gps_filename: str) -> None:
+        self.accelerometer_filename = accelerometer_filename
+        self.gps_filename = gps_filename
+        self.accel_file = None
+        self.gps_file = None
+        self.accel_reader = None
+        self.gps_reader = None
+
+    def startReading(self):
+        self.accel_file = open(self.accelerometer_filename, "r")
+        self.gps_file = open(self.gps_filename, "r")
+
+        self.accel_reader = reader(self.accel_file)
+        self.gps_reader = reader(self.gps_file)
+
+        next(self.accel_reader, None)
+        next(self.gps_reader, None)
 
     def read(self) -> AggregatedData:
-        """Метод повертає дані отримані з датчиків"""
-        return AggregatedData(
-            Accelerometer(1, 2, 3),
-            Gps(4, 5),
-            datetime.now(),
-            config.USER_ID,
-        )
+        try:
+            accel_data = next(self.accel_reader)
+            gps_data = next(self.gps_reader)
 
-    def startReading(self, *args, **kwargs):
-        """Метод повинен викликатись перед початком читання даних"""
+            accelerometer = Accelerometer(
+                x=int(accel_data[0]),
+                y=int(accel_data[1]),
+                z=int(accel_data[2]),
+            )
+            gps = Gps(
+                longitude=float(gps_data[0]),
+                latitude=float(gps_data[1]),
+            )
 
-    def stopReading(self, *args, **kwargs):
-        """Метод повинен викликатись для закінчення читання даних"""
+            return AggregatedData(
+                accelerometer=accelerometer,
+                gps=gps,
+                time=datetime.now(),
+                user_id=config.USER_ID,
+            )
+        except StopIteration:
+            return None
+
+    def stopReading(self):
+        if self.accel_file:
+            self.accel_file.close()
+        if self.gps_file:
+            self.gps_file.close()
