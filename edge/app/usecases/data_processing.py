739534a6ba80a8e91d6a91ff651f39e1335649c6
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2025-04-14 20:09:32 +0300

    feat: implemented lab4

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,18 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    z = agent_data.accelerometer.z
+
+    if abs(z) < 1.5:
+        road_state = "normal"
+    elif 1.5 <= abs(z) < 3.0:
+        road_state = "bump"
+    elif abs(z) >= 3.0:
+        road_state = "hole"
+    else:
+        road_state = "unknown"
+
+    return ProcessedAgentData(
+        road_state=road_state,
+        agent_data=agent_data
+    )
