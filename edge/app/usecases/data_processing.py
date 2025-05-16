24133c0aafc84dd778d72e8720aa8ba1e1925ec9
akaeyuhi <andrvostr0101@gmail.com>
2025-05-16 18:36:01 +0300

    feat: added new sensors processing to edge

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -2,16 +2,60 @@ from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
 
-def process_agent_data(
-    agent_data: AgentData,
-) -> ProcessedAgentData:
+def process_agent_data(agent_data: AgentData) -> ProcessedAgentData:
     """
-    Process agent data and classify the state of the road surface.
-    Parameters:
-        agent_data (AgentData): Agent data that containing accelerometer, GPS, and timestamp.
-    Returns:
-        processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
+    Process all sensors and classify their status along with road_state.
     """
-    data_to_process = ProcessedAgentData(road_state='unknown', agent_data=agent_data)
+    # Classify road state based on vibration magnitude
+    vib_mag = agent_data.vibration.magnitude
+    if vib_mag < 1.0:
+        vibration_road = 'smooth'
+    elif vib_mag < 3.0:
+        vibration_road = 'moderate'
+    else:
+        vibration_road = 'rough'
 
-    return data_to_process
+    # Temperature status
+    temp = agent_data.temperature.value
+    if temp < 0:
+        temp_stat = 'freezing'
+    elif temp < 20:
+        temp_stat = 'cool'
+    elif temp < 30:
+        temp_stat = 'warm'
+    else:
+        temp_stat = 'hot'
+
+    # Humidity status
+    hum = agent_data.humidity.value
+    if hum < 30:
+        hum_stat = 'dry'
+    elif hum < 60:
+        hum_stat = 'normal'
+    else:
+        hum_stat = 'humid'
+
+    # Light status
+    illum = agent_data.light.illumination
+    light_stat = 'dark' if illum < 50 else 'well-lit'
+
+    # Air quality status
+    aqi = agent_data.air_quality.aqi
+    if aqi is None:
+        aq_stat = 'unknown'
+    elif aqi <= 50:
+        aq_stat = 'good'
+    elif aqi <= 100:
+        aq_stat = 'moderate'
+    else:
+        aq_stat = 'poor'
+
+    return ProcessedAgentData(
+        agent_data=agent_data,
+        road_state='unknown',
+        temp_status=temp_stat,
+        humidity_status=hum_stat,
+        vibration_status=vibration_road,
+        light_status=light_stat,
+        air_quality_status=aq_stat,
+    )
