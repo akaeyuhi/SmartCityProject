75339ea2a526d353812d1bae72d1ecd99281e4f9
akaeyuhi <andrvostr0101@gmail.com>
2025-05-16 17:40:19 +0300

    feat: imported schemas

--- agent/src/schema/aggregated_data_schema.py
+++ agent/src/schema/aggregated_data_schema.py
@@ -2,6 +2,11 @@ from marshmallow import Schema, fields
 from schema.accelerometer_schema import AccelerometerSchema
 from schema.gps_schema import GpsSchema
 from schema.parking_schema import ParkingSchema
+from schema.temperature_schema import TemperatureSchema
+from schema.humidity_schema import HumiditySchema
+from schema.vibration_schema import VibrationSchema
+from schema.light_schema import LightSchema
+from schema.air_quality_schema import AirQualitySchema
 
 
 class AggregatedDataSchema(Schema):
